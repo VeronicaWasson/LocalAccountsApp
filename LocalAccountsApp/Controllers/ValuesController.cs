0c7c1292a197904c2a256585f3cc32478a0ff218
Mike Wasson <mwasson@microsoft.com>
2014-10-10 11:21:10 -0700

    Show user name in API response

--- LocalAccountsApp/Controllers/ValuesController.cs
+++ LocalAccountsApp/Controllers/ValuesController.cs
@@ -11,30 +11,10 @@ namespace LocalAccountsApp.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+            var userName = this.RequestContext.Principal.Identity.Name;
+            return String.Format("Hello, {0}.", userName);
         }
     }
 }
